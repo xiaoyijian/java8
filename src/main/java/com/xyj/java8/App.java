f933e08394c5104142ec5223976708dfea1fb433
xiaoyijian <xiaoyijian@youzan.com>
2018-04-12 19:37:39 +0800

    first commit

---
+++ src/main/java/com/xyj/java8/App.java
@@ -0,0 +1,13 @@
+package com.xyj.java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
